6b6261d0328f502237bc223c742a5d748d5c0470
pashynskyi <59894423+pashynskyi@users.noreply.github.com>
2020-09-07 19:51:02 +0300

    Added styles: page-section contacts and install fullpage.js

--- app/js/main.js
+++ app/js/main.js
@@ -14,9 +14,15 @@ $(function () {
     infinite: false,
   });
 
-  $('.questions__item-title').on('click', function(){
+  $('.questions__item-title').on('click', function () {
     $('.questions__item').removeClass('questions__item--active');
     $(this).parent().addClass('questions__item--active');
+  });
+
+  $('#fullpage').fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionSelector: '.page-section',
   });
 
 });
