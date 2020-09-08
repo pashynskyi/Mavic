71b0d82a569d456ca191c99706f1e93476ba9d94
pashynskyi <59894423+pashynskyi@users.noreply.github.com>
2020-09-08 21:19:05 +0300

    Added adaptive styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-
+  // slick-carousel ===================================================================
   $('.products__slider').slick({
     prevArrow: `<button class="slider-btn slider-btn__left">
       <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -13,16 +13,29 @@ $(function () {
     </button>`,
     infinite: false,
   });
-
+  // accordion ======================================================================
   $('.questions__item-title').on('click', function () {
     $('.questions__item').removeClass('questions__item--active');
     $(this).parent().addClass('questions__item--active');
   });
-
+  // fullpage.js ====================================================================
   $('#fullpage').fullpage({
     autoScrolling: true,
     scrollHorizontally: true,
     sectionSelector: '.page-section',
+    scrollOverflow: true,
+    menu: '#header__nav',
+    anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts']
+  });
+  // menu burger ===================================================================
+  $('.menu__btn').on('click', function () {
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__list-link').on('click', function () {
+    $('.menu__btn').removeClass('menu__btn--active');
+    $('.menu__list').removeClass('menu__list--active');
   });
 
 });
